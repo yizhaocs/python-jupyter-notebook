06a93c4bf01d8a445fda222c8b15513fb8d268e5
yzhao <yzhao@fortinet.com>
2022-06-15 12:22:04 -0700

    good

--- pandas_basic_operation.py
+++ pandas_basic_operation.py
@@ -14,12 +14,26 @@ def pandas_shape():
     df = pd.read_csv(input_data, lineterminator='\n')
     print(f"row count:{df.shape[0]}, column count:{df.shape[1]}") # row count:3549, column count:6
 
-def pandas_unique():
+'''
+group by 相同Host name的rows
+'''
+def pandas_groupby():
     input_data = 'Resources/host_health.csv'
     df = pd.read_csv(input_data, lineterminator='\n')
-    unique_host_name = list(df['Host Name'].unique())
-    print(len(unique_host_name))
-    print(unique_host_name)
+    unique_host_name_list = list(df['Host Name'].unique())
+    # 21
+    print(len(unique_host_name_list))
+    # ['FSM-GFU-Window2012R2-WIN2012R2-172-30-56-123', 'ussvnplesx54.fortinet-us.com', 'ussvnplesx58.fortinet-us.com', 'ussvnplesx56.fortinet-us.com', 'ussvnplesx59.fortinet-us.com', 'FSM-GFU1-Window2022-WIN2022-172-30-56-125', 'ussvnplesx51.fortinet-us.com', 'ussvnplesx53.fortinet-us.com', 'ussvnplesx52.fortinet-us.com', 'ussvnplesx57.fortinet-us.com', 'ussvnplesx55.fortinet-us.com', 'FSM-GFU-Window10-WIN10-172-30-56-127', 'FSM-GFU-Window2016-WIN2016test-172-30-56-126.gfu.com', 'WIN-QA-RDP-230', 'FSM-CPWANG-WIND2019-172-30-56-214', 'WIN-2R27C8ADMT3.fortisiem-lab.net', 'FSM-RPRAJUDHA-CENTOS8-NFS217-172.30.57.217', 'fsm-ntwk-wlan01', 'it-rle-oradb-57141', 'FSM-TTRINH-CENTOS8-KAFKA-172.30.56.34', 'HOST-172.30.56.104']
+    print(unique_host_name_list)
+    # row count:3549, column count:6
+    print(f"row count:{df.shape[0]}, column count:{df.shape[1]}")
+
+    for host_name in unique_host_name_list:
+        same_host_name_group_df = df[df['Host Name'] == host_name].reset_index(drop=True)
+        print(f"host_name:{host_name}")
+        print(f"row count:{same_host_name_group_df.shape[0]}, column count:{same_host_name_group_df.shape[1]}")
+        print(same_host_name_group_df['Host Name'])
+
 
 
 def pandas_append_df():
@@ -36,6 +50,6 @@ def pandas_append_df():
 
 
 if __name__ == '__main__':
-    # pandas_unique()
-    pandas_shape()
+    pandas_groupby()
+    # pandas_shape()
     # pandas_append_df()
