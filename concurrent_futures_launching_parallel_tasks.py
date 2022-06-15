c1d67a22140a50df554035d970d9771ae641313e
yzhao <yzhao@fortinet.com>
2022-06-15 14:51:01 -0700

    good

--- concurrent_futures_launching_parallel_tasks.py
+++ concurrent_futures_launching_parallel_tasks.py
@@ -1,6 +1,6 @@
-import concurrent
 import time
-from concurrent.futures import ThreadPoolExecutor
+
+from concurrent import futures
 
 
 def try_my_operation(second):
@@ -13,6 +13,6 @@ def try_my_operation(second):
 
 if __name__ == '__main__':
     seconds = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    executor = concurrent.futures.ProcessPoolExecutor(len(seconds))
-    futures = [executor.submit(try_my_operation, s) for s in seconds]
-    concurrent.futures.wait(futures)
+    executor = futures.ProcessPoolExecutor(len(seconds))
+    task_list = list(executor.submit(try_my_operation, s) for s in seconds)
+    futures.wait(task_list, timeout=None, return_when=futures.ALL_COMPLETED)
