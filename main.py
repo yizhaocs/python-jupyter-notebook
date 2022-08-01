05740629f5d16c585fa01ebeab6ed5eb3f15829c
yzhao <yzhao@fortinet.com>
2022-08-01 14:27:48 -0700

    good

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import datetime
 if __name__ == '__main__':
-    # input_data = 'Resources/internet_traffic_raw_data.csv'
-    # df = pd.read_csv(input_data, lineterminator='\n')
-    # dt1 = datetime.datetime.strptime(df['_time'][0], '%Y-%m-%dT%H:%M:%S')
-    # dt2 = datetime.datetime.strptime(df['_time'][1], '%Y-%m-%dT%H:%M:%S')
-    #
-    dt1 = datetime.datetime.strptime('2015-03-02 00:00:00', '%Y-%m-%d %H:%M:%S')
-    dt2 = datetime.datetime.strptime('2015-03-02 02:00:00', '%Y-%m-%d %H:%M:%S')
-    tdelta = dt2 - dt1
-    print(tdelta)
-    print(type(tdelta))
+    input_data = 'Resources/internet_traffic_raw_data.csv'
+    df = pd.read_csv(input_data, lineterminator='\n')
+    first_datetime = datetime.datetime.strptime(df['_time'][0], '%Y-%m-%dT%H:%M:%S')
+    second_datetime = datetime.datetime.strptime(df['_time'][1], '%Y-%m-%dT%H:%M:%S')
 
-    dt_rng = pd.date_range('2015-03-02 00:00:00', '2015-03-03 23:00:00', freq=tdelta)
+    first_datetime = datetime.datetime.strptime('2015-03-02 00:00:00', '%Y-%m-%d %H:%M:%S')
+    second_datetime = datetime.datetime.strptime('2016-03-02 02:00:00', '%Y-%m-%d %H:%M:%S')
+    tdelta = second_datetime - first_datetime
+    print(f'tdelta:{tdelta}')
+    print(f'type(tdelta):{type(tdelta)}')
+
+    last_datetime = datetime.datetime.strptime(df['_time'][len(df) - 1], '%Y-%m-%dT%H:%M:%S')
+    print(f'last_datetime:{last_datetime}')
+    dt_rng = pd.date_range('2015-03-02 00:00:00', last_datetime, freq=tdelta)
     print(dt_rng)
