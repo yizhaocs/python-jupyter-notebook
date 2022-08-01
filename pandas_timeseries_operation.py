ea1e180e1c5202658456d6f40d0c2df2b8ce6278
yzhao <yzhao@fortinet.com>
2022-08-01 10:45:21 -0700

    good

--- pandas_timeseries_operation.py
+++ pandas_timeseries_operation.py
@@ -3,3 +3,9 @@ import numpy as np
 if __name__ == '__main__':
     dt_rng = pd.date_range('2015-03-02 00:00:00', '2015-03-03 23:00:00', freq='H')
     print(dt_rng)
+
+    dt_rng = pd.date_range('2015-03-02 00:00:00', '2015-03-03 23:00:00', freq='2H')
+    print(dt_rng)
+
+    dt_rng = pd.date_range('2015-03-02 00:00:00', '2015-03-02 3:00:00', freq='15min')
+    print(dt_rng)
