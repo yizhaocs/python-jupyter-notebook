f231f5066e6dafc744c38adf2a2f46f8f38a98af
yzhao <yzhao@fortinet.com>
2022-10-12 10:28:04 -0700

    good

--- sklearn_ex/AbstractAlgo.py
+++ sklearn_ex/AbstractAlgo.py
@@ -94,8 +94,8 @@ class AbstractClassifier(AbstractAlgo):
             for label_col in range(len(labels)):
                 confusion_matrix = confusion_matrix_with_labels[label_col]
                 confusion = {
-                    "True Negative": int(confusion_matrix[0, 0]),
-                    "False Positive": int(confusion_matrix[0, 1]),
+                    # "True Negative": int(confusion_matrix[0, 0]),
+                    # "False Positive": int(confusion_matrix[0, 1]),
                     "False Negative": int(confusion_matrix[1, 0]),
                     "True Positive": int(confusion_matrix[1, 1])
                 }
