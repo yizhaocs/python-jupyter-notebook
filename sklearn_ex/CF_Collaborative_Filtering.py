1e13e6ec82b91d273f89d5da9751c27a9fe96701
yzhao <yzhao@fortinet.com>
2022-10-20 16:08:04 -0700

    3

--- sklearn_ex/CF_Collaborative_Filtering.py
+++ sklearn_ex/CF_Collaborative_Filtering.py
@@ -8,6 +8,8 @@ from sklearn_ex.utils.const_utils import MODEL_TYPE_SINGLE, FITTED_PARAMS, PRIDC
 from sklearn_ex.utils.param_utils import parse_params
 from sklearn.neighbors import NearestNeighbors
 
+
+
 class CF_Collaborative_Filtering(AbstractClassifier):
 
     def __init__(self, options):
@@ -25,6 +27,8 @@ class CF_Collaborative_Filtering(AbstractClassifier):
 
 
     def train(self, df, options):
+        number_neighbors =  options['number_neighbors']
+        n_neighbors = number_neighbors
         feature_attrs = options['feature_attrs']
         target_attr = options['target_attr']
         feature_data = df[feature_attrs]
@@ -52,9 +56,79 @@ class CF_Collaborative_Filtering(AbstractClassifier):
             pd.concat([
                 pd.DataFrame(ss_feature_train),
                 pd.DataFrame(ss_feature_test)
-            ], axis=0))
+            ], axis=0),  n_neighbors=number_neighbors)
         print(f'distances:{distances}')
         print(f'indices:{indices}')
+
+        # convert user_name to user_index
+        user_index = df.columns.tolist().index('User')
+        print(f'user_index:{user_index}')
+
+        df1 = df.copy()
+        # t: movie_title, m: the row number of t in df
+        for m, t in list(enumerate(df.index)):
+
+            # find movies without ratings by user_4
+            if df.iloc[m, user_index] == 0:
+                sim_movies = indices[m].tolist()
+                movie_distances = distances[m].tolist()
+
+                # Generally, this is the case: indices[3] = [3 6 7]. The movie itself is in the first place.
+                # In this case, we take off 3 from the list. Then, indices[3] == [6 7] to have the nearest NEIGHBORS in the list.
+                if m in sim_movies:
+                    id_movie = sim_movies.index(m)
+                    sim_movies.remove(m)
+                    movie_distances.pop(id_movie)
+
+                    # However, if the percentage of ratings in the dataset is very low, there are too many 0s in the dataset.
+                # Some movies have all 0 ratings and the movies with all 0s are considered the same movies by NearestNeighbors().
+                # Then,even the movie itself cannot be included in the indices.
+                # For example, indices[3] = [2 4 7] is possible if movie_2, movie_3, movie_4, and movie_7 have all 0s for their ratings.
+                # In that case, we take off the farthest movie in the list. Therefore, 7 is taken off from the list, then indices[3] == [2 4].
+                else:
+                    sim_movies = sim_movies[:n_neighbors - 1]
+                    movie_distances = movie_distances[:n_neighbors - 1]
+
+                # movie_similarty = 1 - movie_distance
+                movie_similarity = [1 - x for x in movie_distances]
+                movie_similarity_copy = movie_similarity.copy()
+                nominator = 0
+
+                # for each similar movie
+                for s in range(0, len(movie_similarity)):
+
+                    # check if the rating of a similar movie is zero
+                    if df.iloc[sim_movies[s], user_index] == 0:
+
+                        # if the rating is zero, ignore the rating and the similarity in calculating the predicted rating
+                        if len(movie_similarity_copy) == (number_neighbors - 1):
+                            movie_similarity_copy.pop(s)
+
+                        else:
+                            movie_similarity_copy.pop(s - (len(movie_similarity) - len(movie_similarity_copy)))
+
+                    # if the rating is not zero, use the rating and similarity in the calculation
+                    else:
+                        nominator = nominator + movie_similarity[s] * df.iloc[sim_movies[s], user_index]
+
+                # check if the number of the ratings with non-zero is positive
+                if len(movie_similarity_copy) > 0:
+
+                    # check if the sum of the ratings of the similar movies is positive.
+                    if sum(movie_similarity_copy) > 0:
+                        predicted_r = nominator / sum(movie_similarity_copy)
+
+                    # Even if there are some movies for which the ratings are positive, some movies have zero similarity even though they are selected as similar movies.
+                    # in this case, the predicted rating becomes zero as well
+                    else:
+                        predicted_r = 0
+
+                # if all the ratings of the similar movies are zero, then predicted rating should be zero
+                else:
+                    predicted_r = 0
+
+                # place the predicted rating into the copy of the original dataset
+                df1.iloc[m, user_index] = predicted_r
         '''
             metrics = self.evaluate(target_data, y_pred)
             # feature_import = list(self.estimator.feature_importances_.round(DECIMAL_PRECISION))
@@ -76,7 +150,7 @@ if __name__ == '__main__':
     import json
 
 
-    raw_data = pd.read_csv('../Resources/fortinet_reports/report1665512454703.csv')
+    raw_data = pd.read_csv('../Resources/fortinet_reports/report1665512454703_resolution_as_user.csv')
 
     ############################################################################################################################################
     incident_target_parsed = raw_data['Incident Target'].str.split(pat=',', expand=False)
@@ -134,18 +208,14 @@ if __name__ == '__main__':
     techniqueid_data_df = pd.DataFrame(techniqueid_data, columns=['techniqueid'])
     raw_data = pd.concat([raw_data, techniqueid_data_df], axis=1)
     print(f'raw_data.columns:{raw_data.columns}')
-    ##############################################################################################################
-    incident_status = raw_data['Incident Status']
-    incident_resolution = raw_data['Incident Resolution']
-    raw_data['Incident_Status_with_Incident_Resolution'] = incident_status.astype(str) + incident_resolution.astype(str)
-    labels = raw_data["Incident_Status_with_Incident_Resolution"].unique()
-    print(f'labels:{labels}')
+
     ##############################################################################################################
     options = {
         'algo_params': json.dumps({
             "metric": "cosine",
             "algorithm": "brute"
         }),
+        'number_neighbors': 3,
         'feature_attrs': [
             'Event Name',
             'Host IP',
@@ -160,7 +230,7 @@ if __name__ == '__main__':
             'techniqueid'
         ],
         # 'target_attr': 'Incident Status',
-        'target_attr': 'Incident_Status_with_Incident_Resolution',
+        'target_attr': 'User',
         'train_factor': 0.7
     }
     decisiontree_classification = CF_Collaborative_Filtering(options)
