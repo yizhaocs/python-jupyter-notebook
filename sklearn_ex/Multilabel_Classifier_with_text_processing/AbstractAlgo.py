928b86ec45243f7d5bc76d76704fbbd168aa2063
yzhao <yzhao@fortinet.com>
2022-10-31 13:40:47 -0700

    good

---
+++ sklearn_ex/Multilabel_Classifier_with_text_processing/AbstractAlgo.py
@@ -0,0 +1,134 @@
+import pandas as pd
+from sklearn.metrics import accuracy_score, f1_score, \
+    recall_score, precision_score, roc_auc_score, hamming_loss
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn_ex.utils.const_utils import *
+from sklearn_ex.utils.excp_utils import phMLNotImplError
+
+
+class AbstractAlgo(object):
+    """The AbstractAlgo class defines the interface for all phML algorithms."""
+
+    def __init__(self, options):
+        """The initialization function.
+
+        This method is **required**. The __init__ method provides the chance to
+        check grammar, convert parameters passed into the search, and initialize
+        additional objects or imports needed by the algorithm. If none of these
+        things are needed, a simple pass or return is sufficient.
+
+        This will be called before processing any data.
+
+        The `options` argument passed to this method is closely related to the
+        algo implementations. Here is one example with LinearRegression algo:
+            {
+                'algo_name': u'LinearRegression',
+                "algo_params" : {
+                    "feature_attrs" : ["bedrooms", "bathrooms", "sqft_living", "sqft_lot"],
+                    "target_attr" : "price",
+                    "train_factor" : 70
+                }
+            }
+        """
+        self.feature_attrs = []
+        self.target_attr = None
+        msg = 'The {} algorithm cannot be initialized.'
+        msg = msg.format(self.__class__.__name__)
+        raise phMLNotImplError(msg)
+
+    def train(self, df, options):
+        """The train method creates and updates a model - it may make predictions.
+
+        If the algorithm necessarily makes predictions while training, return
+        the output DataFrame here. Additionally, if the algorithm cannot be
+        saved, make predictions and return them. Otherwise, make predictions in
+        the infer method and do not return anything here.
+
+        The `df` argument is a pandas DataFrame from the search or reqport results.
+        Note that modification to `df` within this method will also modify the
+        dataframe to be used in the subsequent infer method.
+
+        The `options` argument is the same as those described in the __init__
+        method.
+        """
+        msg = 'The {} algorithm does not support train.'
+        msg = msg.format(self.__class__.__name__)
+        raise phMLNotImplError(msg)
+
+    def infer(self, df, options):
+        """The infer method creates predictions.
+
+        The `df` argument is a pandas DataFrame from the search results.
+
+        The `options` argument is the same as those described in the __init__
+        method.
+        """
+        msg = 'The {} algorithm does not support apply.'
+        msg = msg.format(self.__class__.__name__)
+        raise phMLNotImplError(msg)
+
+    def evaluate(self, X, y_pred):
+        """The metrics method defines how to evaluate the model.
+        The metrics method is only necessary with a saved model. This method
+        must return dict.
+        """
+        msg = 'The {} algorithm does not support metrics.'
+        msg = msg.format(self.__class__.__name__)
+        raise phMLNotImplError(msg)
+
+
+class AbstractClassifier(AbstractAlgo):
+    def __init__(self):
+        self.ss_feature = StandardScaler()
+
+    def evaluate(self, y_true, y_pred, options = None):
+        # Accuracy
+        acc = accuracy_score(y_true, y_pred)
+
+        # Hamming Loss :Incorrect Predictions
+        # The Lower the result the better
+        ham = hamming_loss(y_true, y_pred)
+
+        from sklearn.metrics import multilabel_confusion_matrix
+        confusion_matrix = multilabel_confusion_matrix(y_true, y_pred)
+        columns = y_true.columns
+        confusion_metrix_dict = {}
+        for index in range(len(confusion_matrix)):
+            single_confusion = confusion_matrix[index]
+            confusion = {
+                "True Negative": int(single_confusion[0, 0]),
+                "False Positive": int(single_confusion[0, 1]),
+                "False Negative": int(single_confusion[1, 0]),
+                "True Positive": int(single_confusion[1, 1])
+            }
+            confusion_metrix_dict[columns[index]] = confusion
+
+        metrics = {"accuracy:": acc, "hamming_score": ham, "confusion_matrix": confusion_metrix_dict}
+        print(f'metrics:{metrics}')
+        return metrics
+
+
+    def infer(self, df, options):
+        model_file = options['model']
+        model = model_file[MODEL_TYPE_SINGLE]
+        feature_attrs = options['feature_attrs']
+
+        feature_data = self.text_preprocessing(df[feature_attrs])
+
+        ohe = OneHotEncoder()
+        feature_data_with_one_hot_encoding = ohe.fit_transform(feature_data).toarray()
+        ss_feature_data = self.ss_feature.fit_transform(feature_data_with_one_hot_encoding)
+        y_pred = model.predict(ss_feature_data)
+        target_attr = options['target_attr']
+        # output = pd.concat([df, pd.DataFrame(y_pred, columns=[f"{PRIDCT_NAME}({target_attr})"])], axis=1)
+
+
+        columns = options['target_attr']
+        predict_columns = []
+        for index in range(len(columns)):
+            predict_columns.append(columns[index] + '_predicted')
+
+        y_pred = y_pred.toarray()
+        output = pd.concat([df, pd.DataFrame(y_pred, columns=predict_columns)], axis=1).reset_index(drop=True)
+
+        return output
---
+++ sklearn_ex/Multilabel_Classifier_with_text_processing/Multilabel_Classifier_with_text_processing.py
@@ -0,0 +1,230 @@
+import pickle
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from skmultilearn.problem_transform import BinaryRelevance
+
+from sklearn_ex.AbstractAlgo import AbstractClassifier
+from sklearn_ex.utils.const_utils import MODEL_TYPE_SINGLE, FITTED_PARAMS, PRIDCT_NAME, DIFF_NAME, DECIMAL_PRECISION
+from sklearn_ex.utils.param_utils import parse_params
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier as _RandomForestClassifier
+from imblearn.ensemble import BalancedBaggingClassifier
+
+
+class Classifier_with_text_processing(AbstractClassifier):
+
+    def __init__(self, options):
+        super(Classifier_with_text_processing, self).__init__()
+
+        algorithm = options['algorithm']
+
+        '''
+            According to my understanding of NB algorithm:
+
+                1.Gaussian NB: It should be used for features in decimal form. GNB assumes features to follow a normal distribution.
+                
+                2.MultiNomial NB: It should be used for the features with discrete values like word count 1,2,3...
+                
+                3.Bernoulli NB: It should be used for features with binary or boolean values like True/False or 0/1.
+        '''
+        self.estimator = BinaryRelevance(GaussianNB())
+
+    def text_preprocessing(self, df):
+        import neattext as nt
+        import neattext.functions as nfx
+
+        # Explore For Noise
+        df['Incident Title'].apply(lambda x: nt.TextFrame(x).noise_scan())
+
+        # Explore For Noise
+        df['Incident Title'].apply(lambda x: nt.TextExtractor(x).extract_stopwords())
+
+        # Explore For Noise
+        df['Incident Title'].apply(nfx.remove_stopwords)
+
+        corpus = df['Incident Title'].apply(nfx.remove_stopwords)
+
+        from sklearn.feature_extraction.text import TfidfVectorizer
+        tfidf = TfidfVectorizer(analyzer='word', stop_words='english')
+        # Build Features
+        Xfeatures = tfidf.fit_transform(corpus).toarray()
+        df_tfidfvect = pd.DataFrame(data=Xfeatures, columns=tfidf.get_feature_names())
+        df = df.drop('Incident Title', axis=1)
+        df = pd.concat([df, df_tfidfvect], axis=1)
+        return df
+
+    def train(self, df, options):
+        feature_attrs = options['feature_attrs']
+        target_attr = options['target_attr']
+        if options['algorithm'] == 'multilabel':
+            feature_data = self.text_preprocessing(df[feature_attrs])
+        else:
+            feature_data = df[feature_attrs]
+
+        target_data = df[target_attr]
+
+        ####################################################################################################
+        ohe = OneHotEncoder()
+        feature_data_with_one_hot_encoding = ohe.fit_transform(feature_data).toarray()
+        ####################################################################################################
+
+        # 1. Split the data randomly with 70:30 of train and test.
+        feature_train, feature_test, target_train, target_test = \
+            train_test_split(feature_data_with_one_hot_encoding, target_data, random_state=42, shuffle=True,
+                             test_size=1 - float(options['train_factor']))
+        self.ss_feature = StandardScaler()
+        # 2. Standardlize the train and test data of features.
+        ss_feature_train = self.ss_feature.fit_transform(feature_train)
+        ss_feature_test = self.ss_feature.fit_transform(feature_test)
+
+        # 3. Train the model with LogisticRegression
+        self.estimator.fit(ss_feature_train, target_train)
+
+        # 4. Evaluate the model performance
+        y_pred = self.estimator.predict(
+            pd.concat([
+                pd.DataFrame(ss_feature_train),
+                pd.DataFrame(ss_feature_test)
+            ], axis=0))
+        # Convert to Array  To See Result
+
+        if options['algorithm'] == 'multilabel':
+            y_pred = y_pred.toarray()
+            # y_pred.to_csv('/Users/yzhao/Documents/ai_for_operational_management/y_pred.csv', index=False)
+
+
+        metrics = None
+        if options is not None and options['algorithm'] == 'multilabel':
+            metrics = self.evaluate(target_data, y_pred, options)
+        else:
+            metrics = self.evaluate(target_data, y_pred, None)
+
+        # feature_import = list(self.estimator.feature_importances_.round(DECIMAL_PRECISION))
+        # fitted_parameter = {feature_attrs[i]: feature_import[i] for i in range(len(feature_attrs))}
+        # metrics[FITTED_PARAMS] = fitted_parameter
+
+        # 5. Handle the return value
+
+        columns = options['target_attr']
+        predict_columns = []
+        for index in range(len(columns)):
+            predict_columns.append(columns[index] + '_predicted')
+        output = pd.concat([df, pd.DataFrame(y_pred, columns=predict_columns)], axis=1).reset_index(drop=True)
+
+        return self.estimator, output, metrics
+
+
+if __name__ == '__main__':
+    ''' This is used for algorithm level test, should be run at the same dir of this file. 
+            python DecisionTreeClassifier.py
+    '''
+    import json
+
+    raw_data = pd.read_csv('../../Resources/fortinet_reports/report1666743279291_with_incident_title_with_username.csv')
+
+    ############################################################################################################################################
+    incident_target_parsed = raw_data['Incident Target'].str.split(pat=',', expand=False)
+    # print(incident_target_parsed.head())
+    # print(incident_target_parsed.iloc[0])
+    # print(incident_target_parsed.iloc[0][0])
+
+    hostIpAddr_data = []
+    hostName_data = []
+    for i in range(len(incident_target_parsed)):
+        e = incident_target_parsed.iloc[i]
+        if isinstance(e, list):
+            # print(f'e:{e}')
+            for i in range(0, len(e)):
+                s = e[i]
+                if 'hostIpAddr' in s:
+                    hostIpAddr_data.append(s.partition(':')[2])
+                    # print(f'hostIpAddr:{s}')
+                elif 'hostName' in s:
+                    hostName_data.append(s.partition(':')[2])
+                    # print(f'hostName:{s}')
+        else:
+            hostIpAddr_data.append(pd.np.nan)
+            hostName_data.append(pd.np.nan)
+
+    hostIpAddr_data_df = pd.DataFrame(hostIpAddr_data, columns=['incident_target_parsed_hostIpAddr'])
+    hostName_data_df = pd.DataFrame(hostName_data, columns=['incident_target_parsed_hostName'])
+
+    raw_data = pd.concat([raw_data, hostIpAddr_data_df], axis=1)
+    raw_data = pd.concat([raw_data, hostName_data_df], axis=1)
+    ############################################################################################################################################
+    attack_tactic_parsed = raw_data['Attack Technique'].str.split(pat=',', expand=False)
+    techniqueid_data = []
+
+    for i in range(len(attack_tactic_parsed)):
+        e = attack_tactic_parsed.iloc[i]
+        if e and isinstance(e, list):
+            # print(f'e:{e}')
+            for i in range(0, len(e)):
+                s = e[i]
+                if 'techniqueid' in s:
+                    import re
+
+                    techniqueid = s.partition(':')[2]
+
+                    list_of_char = ['\"', '}', '\]', ' ']
+                    pattern = '[' + ''.join(list_of_char) + ']'
+                    techniqueid = re.sub(pattern, '', techniqueid)
+                    techniqueid_data.append(techniqueid)
+                    break
+                    # print(f'techniqueid:{s}')
+        else:
+            techniqueid_data.append(pd.np.nan)
+
+    techniqueid_data_df = pd.DataFrame(techniqueid_data, columns=['techniqueid'])
+    raw_data = pd.concat([raw_data, techniqueid_data_df], axis=1)
+    print(f'raw_data.columns:{raw_data.columns}')
+    ##############################################################################################################
+    incident_status = raw_data['Incident Status']
+    incident_resolution = raw_data['Incident Resolution']
+    raw_data['Incident_Status_with_Incident_Resolution'] = incident_status.astype(str) + incident_resolution.astype(str)
+
+
+    ##############################################################################################################
+    options = {
+        'algorithm': 'multilabel',
+        'feature_attrs': [
+            'Event Name',
+            'Host IP',
+            'Host Name',
+            'Incident Title',
+            'Incident Source',
+            'Incident Reporting Device',
+            'incident_target_parsed_hostName',
+            'incident_target_parsed_hostIpAddr',
+            'Incident Category',
+            'DayOfWeek(Event Receive Time)',
+            'HourOfDay(Event Receive Time)',
+            'techniqueid'
+        ],
+        # 'target_attr': 'Incident Status',
+        'target_attr': ['user_A', 'user_B', 'user_C', 'user_D'],
+        'train_factor': 0.7
+    }
+
+
+
+    ##############################################################################################################
+
+
+    decisiontree_classification = Classifier_with_text_processing(options)
+    model, output, metrics = decisiontree_classification.train(raw_data, options)
+    print(output)
+    print(json.dumps(metrics, indent=2))
+
+    output.to_csv('/Users/yzhao/Documents/ai_for_operational_management/ai_for_operational_management_training.csv', index=False)
+
+    infer_data = raw_data.iloc[:, :]
+    # options.update({'model': pickle.dumps(model)})
+    options.update({'model': {MODEL_TYPE_SINGLE: model}})
+    output = decisiontree_classification.infer(infer_data, options)
+    print(output)
+
+    output.to_csv('/Users/yzhao/Documents/ai_for_operational_management/ai_for_operational_management_inference.csv', index=False)
